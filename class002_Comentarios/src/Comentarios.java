e2f6b623f59d87cc7de08088f6e9be1b09ae8990
Samuel Caitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-27 12:54:33 -0300

    Comments in project Java

---
+++ class002_Comentarios/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+
+	public static void main(String[] args) {
+		//  Comentários de única linha
+
+		/*
+		 * Comentários multinhas
+		 */
+
+		/*
+		 *
+		 * Comentários para documentação
+		 *
+		 */
+	}
+}
