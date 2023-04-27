8f72fa3dcb8cb8248bcf67b8f5d218a52008a815
Samuel Caitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-27 12:51:44 -0300

    Hello world

---
+++ class001_Introducao/src/Introducao.java
@@ -0,0 +1,9 @@
+public class Introducao {
+
+	// método main inicia a execução do aplicativo Java
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo!");
+		
+	} // fim do método main
+} // fim da classe Intro
