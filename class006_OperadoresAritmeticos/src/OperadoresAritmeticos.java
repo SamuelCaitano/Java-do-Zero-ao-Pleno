45def40da63d6acce9d6610c0106915d3e7db149
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-27 15:33:00 -0300

    arithmetic operators

--- class006_OperadoresAritmeticos/src/OperadoresAritmeticos.java
+++ class006_OperadoresAritmeticos/src/OperadoresAritmeticos.java
@@ -1,15 +1,15 @@
 public class OperadoresAritmeticos {
 
 	public static void main(String[] args) {
-		
+
 		int a = 10;
 		int b = 5;
 		int c = a - b;
 		int d = c * a;
-		
-		 // efetua o calculo aritmético
+
+		// efetua o calculo aritmético
 		int soma = a * b / c + (d - a);
-		
-        System.out.println(soma);
+
+		System.out.println(soma);
 	}
 }
