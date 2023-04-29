926a9c3219a8a42ad700e68220a732fa33fba4f9
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-29 15:28:11 -0300

    the new switch expression

---
+++ class010_Switch_Case/src/NewSwitchCase.java
@@ -0,0 +1,28 @@
+
+public class NewSwitchCase {
+ 
+	private static int result;
+
+	public static void main(String[] args) {
+
+		final String[] meses = { "JANEIRO", "FEVEREIRO", "MARCO", "ABRIL", 
+								 "MAIO", "JUNHO", "JULHO", "AGORTO", "SETEMBRO", 
+								 "OUTUBRO", "NOVEMBRO", "DEZEMBRO" };
+
+		final String mes = meses[9];
+
+		int dias = getMesLength(mes);
+
+		System.out.println(mes  + " tem " + dias + " dias.");
+	}
+
+	public static int getMesLength(String mes) {
+		
+		return result = switch (mes) {
+		case "JANEIRO", "MARCO", "MAIO", "JULHO", "AGORTO", "OUTUBRO", "DEZEMBRO" -> 31;
+		case "FEVEREIRO" -> 28;
+		case "ABRIL", "JUNHO", "SETEMBRO", "NOVEMBRO" -> 30;
+		default -> 0;
+		}; 
+	}
+}
