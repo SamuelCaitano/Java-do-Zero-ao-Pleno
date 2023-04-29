212ef55c0d3797fd8d1860375bd384e7fe25fcfd
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-29 15:41:27 -0300

    while loop is the most fundamental looping statement in java

---
+++ class011_While/src/While.java
@@ -0,0 +1,14 @@
+public class While {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		int max = 3;
+
+		while ( num <= max) {
+			System.out.println("execução " + num);
+			num++;
+		}
+		
+	}
+}
